db068e7072f819dc2ff10318e82ae44be2d5c54b
Paul Price <price@astro.princeton.edu>
2015-08-28 14:14:05 -0400

    fixup! Working through ingest.

--- skymap.py
+++ skymap.py
@@ -2,4 +2,4 @@ root.skyMap["discrete"].raList = [320.36749197602893]
 root.skyMap["discrete"].decList = [0.3131554006070023]
 root.skyMap["discrete"].radiusList = [1.4388796242707318]
 root.skyMap["discrete"].pixelScale = 0.168
-
+root.skyMap.name = "discrete"
