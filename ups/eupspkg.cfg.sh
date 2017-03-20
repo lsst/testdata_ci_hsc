c8e1793bb62e33142f4d1921e38d311ec6bdc4d9
Paul Price <price@astro.princeton.edu>
2017-03-20 15:03:50 -0400

    add eupspkg.cfg.sh to support build on OSX
    This allows us to work around SIP on OSX.
    
    This apprently breaks python 3 builds, but ci_hsc is already
    broken for python 3.

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,10 @@
+# Work around SIP on MacOSX
+build() {
+    export DYLD_LIBRARY_PATH=$LSST_LIBRARY_PATH
+    python $(which scons) -j$NJOBS prefix="$PREFIX" version="$VERSION" cc="$CC"
+}
+
+install() {
+    clean_old_install
+    install_ups
+}
